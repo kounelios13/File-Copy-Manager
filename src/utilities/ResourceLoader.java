fb58c215a9e58b60dc4dcd3e84965444d00213ac
kounelios13 <pkounelios@gmail.com>
2016-07-31 13:52:57 +0300

    added ResourcesLoader class. Responsible for loading class for PreferencesManager and FileCopyManager

---
+++ src/utilities/ResourceLoader.java
@@ -0,0 +1,41 @@
+package utilities;
+import java.io.*;
+import java.util.*;
+@SuppressWarnings({"unused"})
+public class ResourceLoader {
+
+	/***
+	 * Handle loading of app related files
+	 * 
+	 * 
+	 * */
+	private ObjectInputStream inputStream;
+	private FileInputStream fileStream;
+
+	private String seperator = File.separator+File.separator;
+	private File uiTheme=PreferencesManager.settingsFile,
+		listFile = new File("app"+seperator+"userlist.dat");
+	public Settings getPreferences(){
+		//Needs to be casted on PreferencesManager sinces Settings class is not visible
+		return null;
+	}
+	public ProgramState getAppState(){
+		/**
+		 * 
+		 * Returns the last saved state of the main program
+		 * */
+		ProgramState p=null;
+		try {
+			fileStream = new FileInputStream(listFile);
+			inputStream = new ObjectInputStream(fileStream);
+			p = (ProgramState)inputStream.readObject();
+		} catch (IOException exc) {
+			// TODO Auto-generated catch block
+			exc.printStackTrace();
+		} catch (ClassNotFoundException exc) {
+			// TODO Auto-generated catch block
+			exc.printStackTrace();
+		}
+		return p;
+	}
+}
