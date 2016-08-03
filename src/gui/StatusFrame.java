169d944d9389872fd70e807cab08e2baeb3ac8fe
kounelios13 <pkounelios@gmail.com>
2016-08-03 22:50:24 +0300

    changed background of StatusFrame

--- src/gui/StatusFrame.java
+++ src/gui/StatusFrame.java
@@ -1,15 +1,21 @@
 package gui;
+import java.awt.Color;
 import java.awt.EventQueue;
 
-import javax.swing.*;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import java.awt.Font;
 @SuppressWarnings({"serial"})
 public class StatusFrame extends JFrame{
 	private JPanel panel = new JPanel();
 	private JLabel currentFileLabel = new JLabel("Current File");
 	public StatusFrame(){
-		
+		//currentFileLabel.setFont(getFont().deriveFont((float) 18.0));
+		panel.setBackground(new Color(18,193,17));
+		currentFileLabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		panel.add(currentFileLabel);
-		this.setSize(400,100);
+		this.setSize(600,100);
 		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 		this.setContentPane(panel);
 
@@ -19,9 +25,10 @@ public class StatusFrame extends JFrame{
 	}
 	public StatusFrame text(String s){
 		currentFileLabel.setText("Copying "+s+" .");
+		this.pack();
 		return this;
 	}
 	public static void main(String[] args) {
-		 EventQueue.invokeLater(()->System.out.println("No worries"));
+		 EventQueue.invokeLater(()->new StatusFrame().setVisible(true));
 	}
 }
