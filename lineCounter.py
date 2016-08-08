488c09d6c7ff0c989ef5a4337995cb7dd16cd87c
kounelios13 <pkounelios@gmail.com>
2016-08-09 00:23:09 +0300

    updated message

--- lineCounter.py
+++ lineCounter.py
@@ -1,7 +1,4 @@
-
 num_of_lines = 0
-g = "src/gui/"
-u = "src/utils/"
 # files = [g+"FileCopyManager.java",g+"StatusFrame.java",u+"Controller.java",u+"PreferencesManager.java",u+"ProgramState.java",u+"UIPreferences.java",u+"ResourceLoader.java",u+"FileHandler.java"]
 files=open('feed.txt','r').readlines()
 for e in files:
@@ -9,4 +6,4 @@ for e in files:
 	e=e.rstrip()
 	file = open(e,'r')
 	num_of_lines+=len(file.readlines())
-print("Total lines {} ".format(num_of_lines))	
+print("{} lines of code found in {} files ".format(num_of_lines,len(files)))	
