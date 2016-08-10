dc70a2138e5df79738b61f3a712920181cb83997
kounelios13 <pkounelios@gmail.com>
2016-08-10 17:26:36 +0300

    updated gitignore
    A script thaa counts total lines of specific files that are found in "feed.txt"

---
+++ dummy/lineCounter.py
@@ -0,0 +1,13 @@
+num_of_lines = 0
+# files = [g+"FileCopyManager.java",g+"StatusFrame.java",u+"Controller.java",u+"PreferencesManager.java",u+"ProgramState.java",u+"UIPreferences.java",u+"ResourceLoader.java",u+"FileHandler.java"]
+files=open('feed.txt','r').readlines()
+for e in files:
+	#strip new line character
+	e=e.rstrip()
+	try:
+		file = open(e,'r')
+		num_of_lines+=len(file.readlines())
+	except FileNotFoundError:
+		print("File {} not found".format(e))
+
+print("{} lines of code found in {} files ".format(num_of_lines,len(files)))	
