5ace3bf8a90c05853ead488aa8ee25d16def5d62
kounelios13 <pkounelios@gmail.com>
2016-08-16 16:31:11 +0300

    updated bugs.txt
    in notes.txt you can see total source code lines(with no comments) for each file

--- dummy/lineCounter.py
+++ dummy/lineCounter.py
@@ -1,6 +1,7 @@
 from sys import argv
 # Improve comment check
 # check if a line ends with a comment terminating char
+exportFile = open("../notes.txt",'w')
 def isComment(line):
 	comments=["*","//","// ","*/","* ","/*","/**","#"," #"]
 	ending=[" */","*/","**/","#"," #"]
@@ -15,10 +16,18 @@ files=[file.rstrip() for file in open('feed.txt' if len(argv) < 2 else argv[1],'
 lines = 0
 chars = 0
 for f in files:
+
 	temp_lines = open(f,'r').readlines()
+	row = str(f + " --> Total lines :"+str(len(temp_lines))+" \n")
 	for t_line in temp_lines:
 		t_line = t_line.strip()
 		if len(t_line) > 0 and not isComment(t_line):
 			lines+=1
-			chars+=len(t_line.strip())	
+			chars+=len(t_line.strip())
+	try:
+		exportFile.write(row)
+	except Exception as e:
+		print("Exception found "+e)
+exportFile.close()		
+
 print("Total source code lines {}. Number of source code chars {}".format(lines,chars))			
