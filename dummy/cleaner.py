501920b0c91a02a51357cd7e718a77ca9bf24090
kounelios13 <pkounelios@gmail.com>
2016-08-17 10:30:43 +0300

    cleaner.py
    updates on that script

--- dummy/cleaner.py
+++ dummy/cleaner.py
@@ -1,11 +1,25 @@
 from lineCounter import *
-# for file in files:
-# 	for line in open(file,'r').readlines():
-# 		if isComment(line):
-# 			print(line)
-msg = ""
-for f in files:
-	for l in getLines(f):
-		if not isComment(l):
-			msg+=l 
-print(msg)			
+def emptyLineRemove(fileName):
+	file  = open(fileName,'w')
+	lines = file.readlines()
+	for l in line:
+		l = l.strip()
+		if len(l)>0:
+			file.write(l+"\n")
+	file.close()		
+def removeComments(fileName):
+	pass
+def showMenu():
+	while True:
+		print("What you want to do? Select a number")					
+		print("1:Remove empty lines")
+		print("2:Remove comments")
+		ans = int(input("Type a number:"))
+		fileName = input("Type a filename")
+		if ans is 1:
+			emptyLineRemove(fileName)
+		elif ans is 2:
+			removeComments(fileName)
+		go = input("Press Y to continue or N to exit")
+		if go is not Y:
+			break 	
