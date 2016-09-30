f038a4a73fb55e7b45ddd7d2536f714d78df2db2
kounelios13 <pkounelios@gmail.com>
2016-09-30 23:54:14 +0300

    abstract class
    View class in now abstract

--- src/gui/View.java
+++ src/gui/View.java
@@ -22,7 +22,7 @@ package gui;
 import interfaces.IView;
 import javax.swing.JFrame;
 @SuppressWarnings("serial")
-public  class View extends JFrame implements IView{
+public abstract class View extends JFrame implements IView{
 	public View(String title,int width,int height){
 		super(title);
 		this.setSize(width, height);
