9d792f5b4b0c97ec94054c1583947761c0c54bb6
kounelios13 <pkounelios@gmail.com>
2016-11-12 20:23:04 +0200

    fixed typo

--- src/gui/FileCopyManager.java
+++ src/gui/FileCopyManager.java
@@ -35,7 +35,7 @@ public class FileCopyManager extends ApplicationScreen{
 	public static String appName  = "File Copy Manager v1.6.5.1";
 	public static final Color TRANSPARENT_COLOR = new Color(0,0,0,0);
 	private PreferencesManager pManager 		    = new PreferencesManager(this);
-	private JCheckBoxMenuItem allowDuplicatesOption = new JCheckBoxMenuItem("Allow dupliactes in list");
+	private JCheckBoxMenuItem allowDuplicatesOption = new JCheckBoxMenuItem("Allow duplicates in list");
 	private Controller controller = new Controller();
 	private FileHandler fHandler  = new FileHandler();	
 	private JMenuBar menuBar   	  = new JMenuBar();
